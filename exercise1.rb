d6c3227710be981e669e03a2f521ad2561774d0a
Lena <fatmeow@gmail.com>
2017-11-04 02:36:04 -0400

    Create exercise 1

---
+++ exercise1.rb
@@ -0,0 +1,29 @@
+#Bank Account
+class BankAccount
+
+  def initialize
+    @balance = 0
+    @interest_rate = 0.02
+  end
+
+  def deposit=(amount)
+    @balance += amount
+  end
+
+  def withdraw=(amount)
+    @balance -= amount
+  end
+
+  def gain_interest
+    @balance *= @interest_rate + 1
+    return @balance.round(2)
+  end
+
+
+
+end
+
+account01 = BankAccount.new
+puts account01.deposit = 500.67
+puts account01.withdraw = 200
+puts account01.gain_interest
